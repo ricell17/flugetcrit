58607e9c8708ecf68cf2f7d537a709123aa65eb4
ricell17 <39165278+ricell17@users.noreply.github.com>
2020-07-01 21:53:27 +0700

    Add files via upload

---
+++ fluget.js
@@ -0,0 +1,22 @@
+
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4290698,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4290698&101" alt="" border="0"></a></noscript>
+
+<script type="text/javascript">
+	atOptions = {
+		'key' : '726c463e87922041c3d41b515eb5c8f4',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://yim3eyv5.top/726c463e87922041c3d41b515eb5c8f4/invoke.js"></scr' + 'ipt>');
+</script>
